bd7fcfda7ea968839abb9784d1b7c8a376347f87
Vytautas Sipavičius <vspalevenele@gmail.com>
2020-11-27 10:28:53 +0200

    Update movement tests

---
+++ TowerDefense/XUnitTestProject1/GameFlowOperations/MovementTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TowerDefense.Models.Monster;
+using TowerDefense.Models.Strategies.Movement;
+using Xunit;
+
+namespace UnitsTests.GameFlowOperations
+{
+    public class MovementTests
+    {
+        [Fact]
+        public void TestGroundMovement()
+        {
+            var movement = new GroundMovement();
+            var monster = new Monster { MonsterIndex = 1, Position = 1 };
+
+            movement.Move(monster);
+
+            Assert.True(monster.Position == 2 && monster.MonsterIndex == 1);
+        }
+
+        [Fact]
+        public void TestAirMovement()
+        {
+            var movement = new AirMovement();
+            var monster = new Monster { MonsterIndex = 1, Position = 1 };
+
+            movement.Move(monster);
+
+            Assert.True(monster.Position == 3 && monster.MonsterIndex == 1);
+        }
+
+        [Fact]
+        public void TestUnderGroundMovement()
+        {
+            var movement = new GroundMovement();
+            var monster = new Monster { MonsterIndex = 1, Position = 1 };
+
+            movement.Move(monster);
+
+            Assert.True(monster.Position == 2 && monster.MonsterIndex == 1);
+        }
+    }
+}
