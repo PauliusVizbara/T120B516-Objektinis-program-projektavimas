eb14cd8121e1d016bee23f492e4df8c692b2474e
Aleksandras97 <aleksandrnarusevic1@gmail.com>
2020-12-10 00:48:22 +0200

    State pattern
    added state pattern and changed Monster

---
+++ TowerDefense/TowerDefense/Models/Monster/Alive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TowerDefense.Models.Monster
+{
+    public class Alive : MonsterState
+    {
+
+        public override void Kill()
+        {
+            this.Handle.TransitionTo(new Dead());
+        }
+    }
+}
---
+++ TowerDefense/TowerDefense/Models/Monster/MonsterState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TowerDefense.Models.Monster
+{
+    public abstract class MonsterState
+    {
+        protected Monster Handle { get; set; }
+
+        public void SetMonsterState(Monster Handle)
+        {
+            this.Handle = Handle;
+        }
+
+        public virtual void Kill() { }
+    }
+}
